6a41d2e3e983183cac26a1113cd494f260d70f69
xyro10 <sobhanvaez10@gmail.com>
2026-07-31 15:53:13 +0330

    Add ToDoList class in todo_management.py and implement functions

---
+++ todo_management.py
@@ -0,0 +1,20 @@
+from task import Task
+
+class ToDoList():
+    def __init__(self):
+        self.tasks = []
+
+    def add_task(self): 
+        pass
+
+    def show_tasks(self):
+        pass
+
+    def remove_task(self):
+        pass
+
+    def save_to_csv(self): 
+        pass
+
+    def load_from_csv(self):
+        pass
